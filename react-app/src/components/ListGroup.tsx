da124b9b1e2f2b249c625bffdc440091d11e21fe
saks <sakthikumar.s@publicissapient.com>
2023-04-01 10:15:26 +0530

    Conditional rendering

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,7 +1,18 @@
 const ListGroup = () => {
+  const items = ["New York", "Mumbai", "London", "Tokyo", "Paris"];
   return (
-    <div>ListGroup</div>
-  )
-}
+    <>
+      <h1>List of items</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li className="list-group-item" key={index}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
-export default ListGroup
+export default ListGroup;
