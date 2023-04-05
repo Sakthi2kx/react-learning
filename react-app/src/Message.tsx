a08a8797802f02311ae26dc272b1a9d715677910
saks <sakthikumar.s@publicissapient.com>
2023-04-05 17:53:07 +0530

    updating objects

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 const Message = () => {
   const name = "Sakthi";
-  if (name) return <h1>Hello {name}</h1>;
-  return <h1>Hello World</h1>;
+  const [customer, setCustomer] = useState({
+    name: "Sakthi",
+    address: {
+      city: "salem",
+      pincode: 683757,
+    },
+  });
+
+  const handleClick = () => {
+    setCustomer({ ...customer, address: {...customer.address, pincode: 695949} });
+  };
+
+  return (
+    <>
+      {name ? <h1>Hello {name}</h1> : <h1>Hello World</h1>}
+      <p>Get Updated info: {customer.name} {customer.address.city} {customer.address.pincode}</p>
+      <button onClick={handleClick}> Update </button>
+    </>
+  );
 };
 
 export default Message;
