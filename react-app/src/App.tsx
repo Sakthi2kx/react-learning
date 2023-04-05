15f4b879c98ed9227dff1f95dd4c0f3c2b88b4dc
saks <sakthikumar.s@publicissapient.com>
2023-04-05 19:30:04 +0530

    expandable text exercise

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,8 +2,7 @@ import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 import ListGroup from "./components/ListGroup";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
   const items = ["New York", "Mumbai", "London", "Tokyo", "Paris"];
@@ -35,6 +34,21 @@ function App() {
       {/* to demostrate sharing state between components */}
       {/* <NavBar cartItemsCount={cartItems.length}/>
       <Cart cartItems={cartItems} onClear={() => setCartItems([])}/> */}
+
+      <ExpandableText maxChars={50}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+        necessitatibus totam quaerat dolore nemo ab officia, labore itaque
+        consequuntur cumque id placeat illum laboriosam repellendus culpa vel.
+        Explicabo fugiat a magnam corrupti odio minus eligendi nihil
+        repudiandae, ipsa praesentium doloremque quisquam deleniti totam dolore
+        porro ex, dolorum, nostrum ratione. Vel similique perferendis
+        consectetur officia dolores error quia pariatur officiis soluta deserunt
+        nobis earum velit, magnam eaque quo modi sequi, explicabo adipisci
+        recusandae. Laudantium sit optio accusamus porro sint facere aperiam
+        perferendis voluptate sequi eaque quod, voluptatibus hic eligendi.
+        Deleniti cumque culpa reiciendis id commodi est minima qui vitae
+        possimus sit?
+      </ExpandableText>
     </div>
   );
 }
