a9579ebd0117740da8d7159cc66e02ba61863ffc
saks <sakthikumar.s@publicissapient.com>
2023-04-06 19:20:51 +0530

    react form

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -11,7 +11,7 @@ const Form = () => {
     handleSubmit,
     formState: { errors },
   } = useForm<FormData>();
-  
+
   const onSubmit = (data: FieldValues) => {
     console.log(data);
   };
@@ -54,62 +54,5 @@ const Form = () => {
     </form>
   );
 };
-
-//Using use ref hook
-//   const nameRef = useRef<HTMLInputElement>(null);
-//   const ageRef = useRef<HTMLInputElement>(null);
-//   const person = {name : "", age: 0};
-
-//   const handleSubmit = (event: FormEvent) => {
-//     event.preventDefault();
-//     if (nameRef.current !== null) person.name =  nameRef.current.value;
-//     if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
-//     console.log(person);
-
-// };
-
-//Using use state hook
-//   const [person, setPerson] = useState({ name: "", age: 0 });
-//   const handleSubmit = (event: FormEvent) => {
-//     event.preventDefault();
-//     console.log(person);
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <div className="mb-3">
-//         <label htmlFor="name" className="form-label">
-//           Name
-//         </label>
-//         <input
-//           onChange={(event) =>
-//             setPerson({ ...person, name: event.target.value })
-//           }
-//           value={person.name}
-//           id="name"
-//           type="text"
-//           className="form-control"
-//         />
-//       </div>
-//       <div className="mb-3">
-//         <label htmlFor="age" className="form-label">
-//           Age
-//         </label>
-//         <input
-//           onChange={(event) =>
-//             setPerson({ ...person, age: parseInt(event.target.value) })
-//           }
-//           value={person.age}
-//           id="age"
-//           type="number"
-//           className="form-control"
-//         />
-//       </div>
-//       <button className="btn btn-primary" type="submit">
-//         Submit
-//       </button>
-//     </form>
-//   );
-// };
 
 export default Form;
