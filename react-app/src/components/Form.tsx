8a98508ac79eb59d14c4a1d259ba6bcdfeb1719c
saks <sakthikumar.s@publicissapient.com>
2023-04-06 18:32:11 +0530

    adding validation to input field

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -1,30 +1,48 @@
 import { FieldValues, useForm } from "react-hook-form";
 
+interface FormData {
+  name: string;
+  age: number;
+}
+
 const Form = () => {
-  const{register, handleSubmit} = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+  
   const onSubmit = (data: FieldValues) => {
     console.log(data);
   };
 
-return (
+  return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
         <input
-          {...register('name')}
+          {...register("name", { required: true, minLength: 3 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {errors.name?.type === "required" && (
+          <p className="text-danger">The name field is required</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">
+            The name field must be atleast 3 characters{" "}
+          </p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
         <input
-          {...register('age')}
+          {...register("age")}
           id="age"
           type="number"
           className="form-control"
@@ -36,29 +54,21 @@ return (
     </form>
   );
 };
-                 
 
+//Using use ref hook
+//   const nameRef = useRef<HTMLInputElement>(null);
+//   const ageRef = useRef<HTMLInputElement>(null);
+//   const person = {name : "", age: 0};
 
+//   const handleSubmit = (event: FormEvent) => {
+//     event.preventDefault();
+//     if (nameRef.current !== null) person.name =  nameRef.current.value;
+//     if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+//     console.log(person);
 
+// };
 
-
-
-
-                        //Using use ref hook
-  //   const nameRef = useRef<HTMLInputElement>(null);
-  //   const ageRef = useRef<HTMLInputElement>(null);
-  //   const person = {name : "", age: 0};
-
-  //   const handleSubmit = (event: FormEvent) => {
-  //     event.preventDefault();
-  //     if (nameRef.current !== null) person.name =  nameRef.current.value;
-  //     if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
-  //     console.log(person);
-
-  // };
-
-
-                    //Using use state hook
+//Using use state hook
 //   const [person, setPerson] = useState({ name: "", age: 0 });
 //   const handleSubmit = (event: FormEvent) => {
 //     event.preventDefault();
