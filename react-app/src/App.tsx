f986d3c39bb08d806bd8973c35bd3b1678c24f6f
saks <sakthikumar.s@publicissapient.com>
2023-04-07 18:27:44 +0530

    update request method

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios, { CanceledError } from "axios";
+import { map } from "zod";
 
 interface User {
   id: number;
@@ -55,6 +56,21 @@ function App() {
       });
   };
 
+  const updateUser = (user: User) => {
+    const updatedUser = { ...user, name: user.name + "!" };
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+    const originalUsers = [...users];
+    axios
+      .patch(
+        "http://jsonplaceholder.typicode.com/users/" + user.id,
+        updatedUser
+      )
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
@@ -69,12 +85,20 @@ function App() {
             className="list-group-item d-flex justify-content-between"
           >
             {user.name}
-            <button
-              className="btn btn-outline-danger"
-              onClick={() => deleteUser(user)}
-            >
-              Delete
-            </button>
+            <div>
+              <button
+                className="btn btn-outline-secondary mx-1"
+                onClick={() => updateUser(user)}
+              >
+                Update
+              </button>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>
+            </div>
           </li>
         ))}
       </ul>
