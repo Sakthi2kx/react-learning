d22588663aaa05c4d191da5385538e7ca4b28278
saks <sakthikumar.s@publicissapient.com>
2023-04-07 13:12:26 +0530

    Expense tracker

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -5,6 +5,10 @@ import ListGroup from "./components/ListGroup";
 import ExpandableText from "./components/ExpandableText";
 import Form from "./components/Form";
 import FormZod from "./components/FormZod";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import categories from "./expense-tracker/categories";
 
 function App() {
   const items = ["New York", "Mumbai", "London", "Tokyo", "Paris"];
@@ -15,6 +19,20 @@ function App() {
   const handleSelectedItem = (item: String) => {
     console.log(item);
   };
+
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "Milk", amount: 5, category: "Groceries" },
+    { id: 2, description: "Egg", amount: 10, category: "Groceries" },
+    { id: 3, description: "Electricity", amount: 100, category: "Utilities" },
+    { id: 4, description: "Movies", amount: 15, category: "Entertainment" },
+  ]);
+
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
   return (
     <div>
       {/* <ListGroup
@@ -52,7 +70,25 @@ function App() {
         possimus sit?
       </ExpandableText> */}
 
-      <FormZod/>
+      {/* <FormZod/> */}
+      <div className="mb-5">
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+      {expenses.length !== 0 ? (
+        <ExpenseList
+          expenses={visibleExpenses}
+          onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+        />
+      ) : null}
     </div>
   );
 }
